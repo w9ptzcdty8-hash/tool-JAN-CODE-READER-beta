a564453d3f6d58d21dfa099fa2f453cde8c4eed7
MRS WORKS <w9ptzcdty8@privaterelay.appleid.com>
2026-08-15 11:03:58 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// ========================================
+// MRS WORKS
+// Main JavaScript
+// ========================================
+
+document.addEventListener("DOMContentLoaded", () => {
+    init();
+});
+
+
+// ========================================
+// Initialization
+// ========================================
+
+function init() {
+    console.log("MRS WORKS initialized");
+
+    // アプリの初期化処理
+}
